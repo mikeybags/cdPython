00f8ee5a76e76a5c544792476a985e30bb18361c
Michael Mitchell <mikeybags@gmail.com>
2017-01-12 20:49:36 -0600

    completed ninja_gold and fixed various bugs

--- flask_fun/ninja_gold/server.py
+++ flask_fun/ninja_gold/server.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, flash
+import random
+import datetime
 
 app = Flask(__name__)
-app.secret_key = "9cf8cf0715076c1324f38046a2ec75d3"
+app.secret_key = "9cf8cf0715076c1324f38046a2ec75d4"
 
 @app.route('/')
 def index():
@@ -9,50 +11,51 @@ def index():
         session['gold'] = 0
     if 'activity_log' not in session:
         session['activity_log'] = []
-    return render_template("index.html", gold = session['gold'])
+    return render_template("index.html")
 
 @app.route('/process_money', methods=["POST"])
 def process_money():
-    import random
-    import datetime
     choice = request.form["choice"]
     timestamp = str(datetime.datetime.now().strftime("%Y/%m/%d %I:%M %p"))
     if choice == 'farm':
         earnings = random.randrange(10,21)
         session['gold'] += earnings
-        activity_log = 'Earned ' +  str(earnings) + ' pieces of gold from the farm! ' + timestamp
+        activity_log = 'Earned ' +  str(earnings) + ' pieces of gold from the farm! (' + timestamp + ')'
         session['activity_log'].append(activity_log)
     elif choice == 'cave':
         earnings = random.randrange(5,11)
         session['gold'] += earnings
-        activity_log = 'Found ' + str(earnings) + ' pieces of gold in the cave! ' + timestamp
+        activity_log = 'Found ' + str(earnings) + ' pieces of gold in the cave! (' + timestamp + ')'
         session['activity_log'].append(activity_log)
     elif choice == 'house':
         earnings = random.randrange(2, 6)
         session['gold'] += earnings
-        activity_log = 'Found ' + str(earnings) + ' pieces of gold in the couch cushions! ' + timestamp
+        activity_log = 'Found ' + str(earnings) + ' pieces of gold in the couch cushions! (' + timestamp + ')'
         session['activity_log'].append(activity_log)
     else:
-        results = random.randrange(-50, 51)
-        session['gold'] += results
-        if results < 0:
-            if (session['gold'] + results) <= 0:
-                results = session['gold'] - results
-                session['gold'] = 0
-                activity_log = 'Lost all ' + str(results) + ' pieces of gold at the casino! :-( ' + timestamp
+        if session['gold'] == 0:
+            flash('You have no gold to risk at the casino! (' + timestamp + ')')
+        else:
+            results = random.randrange(-50,51)
+            print "result is ", results
+            if results < 0:
+                if ((session['gold'] + results) <= 0):
+                    activity_log = 'Lost all ' + str(session['gold']) + ' pieces of gold at the casino! :-(  (' + timestamp + ')'
+                    session['gold'] = 0
+                    session['activity_log'].append(activity_log)
+                else:
+                    session['gold'] += results
+                    results *= -1
+                    activity_log = 'Lost ' + str(results) + ' pieces of gold at the casino! :-(  (' + timestamp + ')'
+                    session['activity_log'].append(activity_log)
+            elif results > 0:
+                session['gold'] += results
+                activity_log = 'Won ' + str(results) + ' pieces of gold at the casino! Woot!  (' + timestamp + ')'
                 session['activity_log'].append(activity_log)
             else:
-                session['gold'] += results
-                results *= -1
-                activity_log = 'Lost ' + str(results) + ' pieces of gold at the casino! :-( ' + timestamp
+                session['gold'] += 0
+                activity_log = 'Came out even at the casino! Try again?  (' + timestamp + ')'
                 session['activity_log'].append(activity_log)
-        elif results > 0:
-            activity_log = 'Won ' + str(results) + ' pieces of gold at the casino! Woot! ' + timestamp
-            session['activity_log'].append(activity_log)
-        else:
-            activity_log = 'Came out even at the casino! Try again? ' + timestamp
-            session['activity_log'].append(activity_log)
     return redirect('/')
 
-# if you have 12 gold and your result is -25, you would be at -13. -13 + -25
 app.run(debug=True)
