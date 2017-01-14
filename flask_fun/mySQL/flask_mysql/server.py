47db0e5e0b856c5be8903b3789a3677832bf93d9
Michael Mitchell <mikeybags@gmail.com>
2017-01-13 19:42:02 -0600

    full friends added

---
+++ flask_fun/mySQL/flask_mysql/server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from mysqlconnection import MySQLConnector
+
+app = Flask(__name__)
+
+mysql = MySQLConnector(app, 'world')
+
+print mysql.query_db("SELECT name FROM countries")
+app.run(debug=True)
