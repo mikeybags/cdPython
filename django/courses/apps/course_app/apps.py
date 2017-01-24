5f4481d27e9dce597df49f876c0b1ae33532fce2
Michael Mitchell <mikeybags@gmail.com>
2017-01-24 11:41:11 -0600

    fixed errors on courses

--- django/courses/apps/course_app/apps.py
+++ django/courses/apps/course_app/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class CoursesAppConfig(AppConfig):
-    name = 'courses_app'
+class CourseAppConfig(AppConfig):
+    name = 'course_app'
