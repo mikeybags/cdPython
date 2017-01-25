2979095a4259e7d641bddf84e22a283633f108fa
Michael Mitchell <mikeybags@gmail.com>
2017-01-25 16:45:22 -0600

    improved login_registration

--- django/login_registration/apps/login_app/views.py
+++ django/login_registration/apps/login_app/views.py
@@ -13,12 +13,14 @@ def register(request):
         email = request.POST['email']
         password = request.POST['password']
         confirm_pw = request.POST['confirm_pw']
+        request.session['first_name'] = first_name
+        request.session['last_name'] = last_name
+        request.session['email'] = email
         errors = []
         errors += User.objects.validate(first_name, last_name, email, password, confirm_pw)
         if not errors:
             errors = User.objects.register(first_name, last_name, email, password)
             if not errors:
-                request.session['first_name'] = first_name
                 return redirect('/success')
             for error in errors:
                 messages.error(request, error)
@@ -30,14 +32,15 @@ def register(request):
 def login(request):
     if request.method == "POST":
         email = request.POST['email']
+        request.session['login_email'] = email
         password = request.POST['password']
         login = User.objects.authenticate(email, password)
         if 'errors' in login:
             for error in login['errors']:
                 messages.error(request, error)
-            return redirect('/')
-        request.session['first_name'] = login['user'].first_name
-        return redirect('/success')
+        else:
+            request.session['first_name'] = login['user'].first_name
+            return redirect('/success')
     return redirect('/')
 
 def logout(request):
